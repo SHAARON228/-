ba8a8fd3b19cf260b208eee8422222f39664d6d4
SHAARON228 <129669155+SHAARON228@users.noreply.github.com>
2023-05-21 17:07:09 +0300

    Create jsbin.js

---
+++ jsbin.js
@@ -0,0 +1,20 @@
+function set_red_background(){
+  document.getElementById('title').innerHTML = "red"; 
+  document.body.style.backgroundColor="red";
+}
+function set_blue_background(){
+document.getElementById('title').innerHTML = "blue"; 
+  document.body.style.backgroundColor="blue";
+}
+function set_yellow_background(){
+   document.getElementById('title').innerHTML = "yellow"; 
+  document.body.style.backgroundColor="yellow";
+}
+function set_D06351_background(){
+  document.getElementById('title').innerHTML = "#D06351"; 
+  document.body.style.backgroundColor="#D06351";
+}
+function set_9FBD17_background(){
+  document.getElementById('title').innerHTML = "#9FBD17"; 
+  document.body.style.backgroundColor="#9FBD17";
+}
